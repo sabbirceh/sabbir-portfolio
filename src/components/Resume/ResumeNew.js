d3582b3fb8d7aff6d9a4f7af3b68a81611fd6bed
Sabbir Salehin <100703179+sabbirceh@users.noreply.github.com>
2023-06-19 16:10:28 +0600

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,10 +1,10 @@
-import pdf from "https://raw.githubusercontent.com/sabbirceh/sabbirceh/main/CV_Sabbir_Salehin.pdf";
 import React, { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import pdf from "../../Assets/CV_Sabbir_Salehin.pdf";
 import Particle from "../Particle";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
