35118ddc5177fe4e28496af1185b21f41133873c
Sabbir Salehin <100703179+sabbirceh@users.noreply.github.com>
2023-06-19 16:59:45 +0600

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -31,7 +31,7 @@ function ResumeNew() {
           </Button>
         </Row>
 
-        <iframe src="https://drive.google.com/file/d/1JWVVZW07hbfF26zlj1LwMvuWTS4CfDFW/preview" width="640" height="900" style="border:1px solid white;" scrolling="yes" allow="autoplay"></iframe>
+        <iframe src="https://drive.google.com/file/d/1JWVVZW07hbfF26zlj1LwMvuWTS4CfDFW/preview" width="640" height="900" border="1px" scrolling="yes" allow="autoplay"></iframe>
 
 
         
