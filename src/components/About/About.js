004c003ce6a9bfa2dd9269e7b447d013a70127f8
Sabbir Salehin <100703179+sabbirceh@users.noreply.github.com>
2023-06-19 12:41:56 +0600

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import laptopImg from "../../Assets/about.png";
 import Particle from "../Particle";
+import Aboutcard from "./AboutCard";
 import Github from "./Github";
 import Techstack from "./Techstack";
-import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about.png";
 import Toolstack from "./Toolstack";
 
 function About() {
@@ -22,7 +22,7 @@ function About() {
             }}
           >
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Know Who <strong className="purple">I'M</strong>
+              Know Wcho <strong className="purple">I'M</strong>
             </h1>
             <Aboutcard />
           </Col>
