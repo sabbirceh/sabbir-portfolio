bc4e1cba5fc6502497df1cc2c87e1adf84878f92
Sabbir Salehin <100703179+sabbirceh@users.noreply.github.com>
2023-06-19 16:50:53 +0600

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,15 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
-import { pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import pdf from "../../Assets/CV_Sabbir_Salehin.pdf";
 import Particle from "../Particle";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+ 
 
-const resumeLink =
-  "https://raw.githubusercontent.com/sabbirceh/sabbirceh/main/CV_Sabbir_Salehin.pdf";
-
+ 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
@@ -34,7 +31,7 @@ function ResumeNew() {
           </Button>
         </Row>
 
-        <iframe src="https://drive.google.com/file/d/1JWVVZW07hbfF26zlj1LwMvuWTS4CfDFW/preview" width="100%" height="980" scrolling="yes" allow="autoplay"></iframe>
+        <iframe src="https://drive.google.com/file/d/1JWVVZW07hbfF26zlj1LwMvuWTS4CfDFW/preview" width="100%" height="auto" scrolling="yes" allow="autoplay"></iframe>
 
 
         
